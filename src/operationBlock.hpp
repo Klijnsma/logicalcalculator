77d752b373b8539978a3b50ab23f9528c675dae6
Jesse Klijnsma <jdg@famklijnsma.nl>
2025-04-11 13:17:45 +0200

    Added operationBlock destructor

--- src/operationBlock.hpp
+++ src/operationBlock.hpp
@@ -8,13 +8,17 @@
 #define IMPLICATION 3
 
 class operationBlock {
-    void* variables = malloc(sizeof(char[2]));
+    void* variables = malloc(sizeof(char) * 2);
 public:
     operationBlock(char variable1, int operation_p, char variable2) {
         *(char*)variables = variable1;
         *((char*)variables + sizeof(char)) = variable2;
 
         operation = operation_p;
+    }
+
+    ~operationBlock() {
+        free(variables);
     }
 
     char operator[](int index) {
