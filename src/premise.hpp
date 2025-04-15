1dea84fa1b4a87633a1020c2259d86f72c3fd7e0
Jesse Klijnsma <jdg@famklijnsma.nl>
2025-04-15 17:42:30 +0200

    added premise class initialization

--- src/premise.hpp
+++ src/premise.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 
 #include "operationBlock.hpp"
@@ -7,5 +8,19 @@
 class premise {
     static std::vector<char> variables;
 public:
+    premise(std::vector<operationBlock> operations_p) {
+        for (int i = 0; i < operations_p.size(); i++) {
+            if (std::find(variables.begin(), variables.end(), operations_p[i][0]) != variables.end()) {
+                variables.push_back(operations_p[i][0]);
+            }
 
+            if (std::find(variables.begin(), variables.end(), operations_p[i][1]) != variables.end()) {
+                variables.push_back(operations_p[i][1]);
+            }
+
+            operations = operations_p;
+        }
+    }
+
+    std::vector<operationBlock> operations;
 };
