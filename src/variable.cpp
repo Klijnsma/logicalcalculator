7ae7c3b89cf4e32e6e19c9e5e3d187f0fbdf6e48
Jesse Klijnsma <jesse@famklijnsma.nl>
2025-06-12 00:51:40 +0200

    Add variableExists function to variable class returning pointer to variable or nullptr.

--- src/variable.cpp
+++ src/variable.cpp
@@ -20,13 +20,21 @@ bool variable::calculate(truthTable* p_truthTable, int row) const {
     return p_truthTable->getTruthValue(this, row);
 }
 
-int variable::getTruthFunctionCount() const {
-    return 0;
-}
-
 std::string variable::getString() const {
     std::string variableString;
     variableString += variableCharacter;
 
     return variableString;
+}
+
+int variable::getTruthFunctionCount() const {
+    return 0;
+}
+
+variable* variable::variableExists(char p_variableCharacter) {
+    for (int variable = 0; variable < s_allVariables.size(); variable++) {
+        if (s_allVariables[variable]->variableCharacter == p_variableCharacter)
+            return s_allVariables[variable];
+    }
+    return nullptr;
 }
--- src/variable.hpp
+++ src/variable.hpp
@@ -5,8 +5,9 @@ public:
     variable(char p_variableCharacter);
 
     bool calculate(truthTable* p_truthTable, int row) const override;
-    int getTruthFunctionCount() const override;
     std::string getString() const override;
+    int getTruthFunctionCount() const override;
+    static variable* variableExists(char p_variableCharacter);
 
     const char variableCharacter;
 };
