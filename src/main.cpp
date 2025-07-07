31189bec9c856937de18fba3165228439859e610
Jesse Klijnsma <jesse@famklijnsma.nl>
2025-07-08 01:32:20 +0200

    Add command line parameter handling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
+#include <cstring>
+#include <filesystem>
+#include <iostream>
+
 #include "csvParsing.hpp"
 #include "truthTable.hpp"
 
-int main() {
-    std::unique_ptr<argumentData> formulae = csvParsing::parseFile("test.csv");
+int main(const int argv, const char** argc) {
+    std::unique_ptr<argumentData> formulae;
+
+    if (argv <= 1)
+        return 1;
+
+    for (int argument = 1; argument < argv; argument++) {
+        if (strcmp(argc[argument], "-i") == 0 && argument + 1 < argv) {
+
+            if (!std::filesystem::exists(argc[argument + 1]) || !std::filesystem::is_regular_file(argc[argument + 1])) {
+                std::cout << "Invalid file \"" << argc[argument + 1] << "\".\n";
+                return 1;
+            }
+
+            formulae = csvParsing::parseFile(argc[argument + 1]);
+            argument++;
+        }
+        else {
+            std::cout << "Invalid argument \"" << argc[argument] << "\".\n";
+            return 1;
+        }
+    }
 
     if (!formulae->premises.empty() && formulae->conclusion != nullptr) {
         truthTable wow(formulae->premises, formulae->conclusion);
